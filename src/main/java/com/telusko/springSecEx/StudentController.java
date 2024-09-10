aea87a8b0525c604ff3b6846388d0e480adb21ac
Brandon Carlos <brandon_marzo@hotmail.com>
2024-09-10 10:46:59 -0300

    Creating fake students to testing get method with authenticantion providing user and password

--- src/main/java/com/telusko/springSecEx/StudentController.java
+++ src/main/java/com/telusko/springSecEx/StudentController.java
@@ -1,10 +1,22 @@
 package com.telusko.springSecEx;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "navin", 60),
+            new Student(2, "Kiran", 65)
+    ));
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
 
 }
