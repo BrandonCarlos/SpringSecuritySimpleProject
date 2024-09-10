3239992e888995edeae90f88f21fb5f6cf69f236
Brandon Carlos <brandon_marzo@hotmail.com>
2024-09-10 13:56:14 -0300

    Generating sessions Id every time that I access a new route

--- src/main/java/com/telusko/springSecEx/config/SecurityConfig.java
+++ src/main/java/com/telusko/springSecEx/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +17,9 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-
+//        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//with this always we will go have another session ID, every time that I log in
 
         return http.build();
     }
