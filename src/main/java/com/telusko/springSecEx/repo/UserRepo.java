3b534e566a68069f5a7a2c167de4c2296eac7fea
Brandon Carlos <brandon_marzo@hotmail.com>
2024-09-10 15:51:23 -0300

    Getting credentials from database to log in and access endpoints

--- src/main/java/com/telusko/springSecEx/repo/UserRepo.java
+++ src/main/java/com/telusko/springSecEx/repo/UserRepo.java
@@ -1,7 +1,13 @@
 package com.telusko.springSecEx.repo;
 
 import com.telusko.springSecEx.model.Student;
+import com.telusko.springSecEx.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+
 }
--- src/main/java/com/telusko/springSecEx/service/MyUserDetailsService.java
+++ src/main/java/com/telusko/springSecEx/service/MyUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.telusko.springSecEx.service;
 
+import com.telusko.springSecEx.model.UserPrincipal;
+import com.telusko.springSecEx.model.Users;
+import com.telusko.springSecEx.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Users user = repo.findByUsername(username);
+
+        if(user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
     }
 }
